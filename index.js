37e7b68df455afb79e25cc9ddf4b83ab5dfa2c69
yhatibi <yhatibi@gmail.com>
2021-03-15 19:49:06 +0100

    asd

--- index.js
+++ index.js
@@ -8,6 +8,33 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/mensajes', (req, res) => {
-    res.send("hello World");
+    res.send(JSON.stringify(  {
+      "glossary": {
+          "title": "example glossary",
+      "GlossDiv": {
+              "title": "S",
+        "GlossList": {
+                  "GlossEntry": {
+                      "ID": "SGML",
+            "SortAs": "SGML",
+            "GlossTerm": "Standard Generalized Markup Language",
+            "Acronym": "SGML",
+            "Abbrev": "ISO 8879:1986",
+            "GlossDef": {
+                          "para": "A meta-markup language, used to create markup languages such as DocBook.",
+              "GlossSeeAlso": ["GML", "XML"]
+                      },
+            "GlossSee": "markup"
+                  }
+              }
+          }
+      }
+  }));
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+/*
+
+
+*/
